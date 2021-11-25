4d61fb210728fe793a172843fb96fae42141ccd1
EricCoton <94869742+EricCoton@users.noreply.github.com>
2021-11-25 17:06:06 -0500

    Update Assignment01.sql

--- Assignment01.sql
+++ Assignment01.sql
@@ -1,24 +1,30 @@
 --List people's full name, fax, phonenumber, company's phonenumber, and company's faxnumber if any
---Get data for people not working for client company
-SELECT FullName, p.PhoneNumber AS PhoneNumber,p.FaxNumber AS FaxNumber, c.PhoneNumber AS CompanyPhoneNumber, c.FaxNumber AS CompanyFaxNumber
+
+SELECT a.FullName, a.PhoneNumber, a.FaxNumber, b.CompanyPhoneNumber, b.CompanyFaxNumber 
+FROM Application.people AS a
+LEFT JOIN     --get all the people
+(SELECT p.PersonID, FullName, p.PhoneNumber AS PhoneNumber,p.FaxNumber AS FaxNumber, c.PhoneNumber AS CompanyPhoneNumber, c.FaxNumber AS CompanyFaxNumber
 FROM Application.People AS p
-LEFT JOIN Sales.Customers AS c
+JOIN Sales.Customers AS c
 ON p.PersonID = c.PrimaryContactPersonID
+WHERE IsEmployee = 0               --people working for customer company
+UNION
 
-
-UNION 
-
---Get data for WWI employee
-SELECT FullName, PhoneNumber, FaxNumber, PhoneNumber AS CompanyPhoneNumber, FaxNumber AS CompanyFaxNumber
+SELECT PersonID, FullName, PhoneNumber, FaxNumber, PhoneNumber AS CompanyPhoneNumber, FaxNumber AS CompanyFaxNumber
 FROM Application.People
-WHERE IsEmployee = 1
+WHERE IsEmployee = 1              --people working for WWI
 
 UNION 
 
---Get data for people working for supply company
-SELECT FullName, p.PhoneNumber AS PhoneNumber,p.FaxNumber AS FaxNumber, s.PhoneNumber AS CompanyPhoneNumber, s.FaxNumber AS CompanyFaxNumber
+SELECT p.PersonID, FullName, p.PhoneNumber AS PhoneNumber,p.FaxNumber AS FaxNumber, s.PhoneNumber AS CompanyPhoneNumber, s.FaxNumber AS CompanyFaxNumber
 FROM Application.People AS p
 join Purchasing.Suppliers AS s
 ON p.PersonID = s.PrimaryContactPersonID
+WHERE IsEmployee = 0 ) AS b       --people working for supplier company 
+ON a.PersonID = b.PersonID 
+WHERE a.PhoneNumber IS NOT NULL 
+
+
+
 
 
