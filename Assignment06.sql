73aa196e1151a6b4c07ead5326a28b832e0c35fc
EricCoton <94869742+EricCoton@users.noreply.github.com>
2022-02-26 17:32:23 -0500

    Update Assignment06.sql

--- Assignment06.sql
+++ Assignment06.sql
@@ -1,10 +1,9 @@
 --List of stock items that are not sold to the state of Alabama and Georgia in 2014
-SELECT StockItemID
-FROM Sales.OrderLines AS l
-join Sales.Orders AS o
-ON l.OrderID = o.OrderID
-WHERE o.OrderDate > '20131231' and o.OrderDate < '20150101' 
 
+USE WideWorldImporters;
+
+SELECT StockItemID         --solution 1 with set operator
+FROM Warehouse.StockItems
 
 EXCEPT
 
@@ -18,7 +17,27 @@ JOIN Application.Cities AS t
 ON c.PostalCityID = t.CityID
 JOIN Application.StateProvinces AS s
 ON t.StateProvinceID = s.StateProvinceID 
+WHERE s.StateProvinceName IN ('Alabama', 'Georgia') AND o.OrderDate > '20131231' AND o.OrderDate < '20150101'
+
+
+SELECT StockItemID         --solution 2 with subquery 
+FROM Warehouse.StockItems
+WHERE StockItemID NOT IN 
+(SELECT StockItemID
+FROM Sales.OrderLines AS l
+JOIN Sales.Orders AS o
+ON l.OrderLineID = o.OrderID
+JOIN Sales.Customers AS c
+ON o.CustomerID = c.CustomerID
+JOIN Application.Cities AS t
+ON c.PostalCityID = t.CityID
+JOIN Application.StateProvinces AS s
+ON t.StateProvinceID = s.StateProvinceID 
 AND s.StateProvinceName IN ('Alabama', 'Georgia')
-WHERE o.OrderDate > '20131231' and o.OrderDate < '20150101'
+WHERE o.OrderDate > '20131231' and o.OrderDate < '20150101')
+
+
+
+
 
 
