0de10bbe4d628a3e5ed0677450c9de5a2d05a21a
EricCoton <94869742+EricCoton@users.noreply.github.com>
2022-02-26 17:08:51 -0500

    Update Assignment03.sql

--- Assignment03.sql
+++ Assignment03.sql
@@ -1,14 +1,15 @@
 --List of customers to whom we made a sale prior to 2016 but no sale since 2016-01-01.
+
+USE WideWorldImporters;
+
 WITH Customers_2016(Customerid) AS
 (SELECT CustomerID FROM Sales.Orders
 WHERE OrderDate < '20160101' 
-GROUP BY CustomerID
 
 EXCEPT
 
 SELECT Customerid  FROM Sales.Orders
 WHERE OrderDate >'20151231'
-GROUP BY CustomerID
 )
 
 SELECT c.Customerid, s.CustomerName
