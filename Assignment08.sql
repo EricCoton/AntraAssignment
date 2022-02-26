1eb3d1ca3cb720e04222db6804a163613a6e0054
EricCoton <94869742+EricCoton@users.noreply.github.com>
2022-02-26 18:44:41 -0500

    Update Assignment08.sql

--- Assignment08.sql
+++ Assignment08.sql
@@ -1,10 +1,13 @@
 --use CTE to get average processing days for each month by each state
-WITH PivotSouce 
+
+USE WideWorldImporters;
+
+WITH PivotSource                   --solution 1: hard coded pivot table
 AS 
-(SELECT DISTINCT s.StateProvinceName,MONTH(o.OrderDate) AS Order_by_Month,
+(SELECT DISTINCT s.StateProvinceName,LEFT(DATENAME(MM, o.OrderDate),3)  AS OrderByMonth
 AVG(DATEDIFF(DAY, o.Orderdate, i.ConfirmedDeliveryTime))
 OVER(PARTITION BY s.StateProvinceName, MONTH(o.orderdate))
-AS Avg_Date
+AS AvgDate
 FROM Sales.Invoices AS i
 JOIN Sales.Orders AS o
 ON i.OrderID = o.OrderID
@@ -17,13 +20,41 @@ ON t.StateProvinceID = s.StateProvinceID )
 
 --pivot table to make months become columns
 
-SELECT StateProvinceName,
-[1] AS JAN, [2] AS FEB, [3] AS March, [4] AS APRIL, [5] AS MAY, 
-[6] AS JUN, [7] AS JUL , [8] AS  AUG, [9] AS [SEP], [10] AS OCT, 
-[11] AS NOV, [12] AS DEC
-FROM PivotSouce
+SELECT *
+FROM PivotSource
 PIVOT (
-MAX(AVG_DATE)
-FOR ORDER_BY_MONTH in ([1], [2], [3], [4], [5], [6], [7], [8],[9],[10],[11],[12]))
+MAX(AvgDATE)
+FOR OrderByMonth in (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec))
 AS p
 ORDER BY StateProvinceName
+GO 
+
+---use CTE to get average processing days for each month by each state
+
+USE WideWorldImporters;                  --solution 2: using dynamic pivot table 
+GO
+DROP VIEW IF EXISTS VW_PivotSource
+GO
+CREATE VIEW VW_PivotSource 
+AS 
+SELECT DISTINCT s.StateProvinceName,LEFT(DATENAME(MM, o.OrderDate),3)  AS OrderByMonth,
+AVG(DATEDIFF(DAY, o.Orderdate, i.ConfirmedDeliveryTime))
+OVER(PARTITION BY s.StateProvinceName, MONTH(o.orderdate))
+AS AvgDate
+FROM Sales.Invoices AS i
+JOIN Sales.Orders AS o
+ON i.OrderID = o.OrderID
+JOIN Sales.Customers AS c
+ON o.CustomerID = c.CustomerID
+JOIN Application.Cities AS t
+ON c.PostalCityID = t.CityID
+JOIN Application.StateProvinces AS s
+ON t.StateProvinceID = s.StateProvinceID
+
+DECLARE @sql AS NVARCHAR(5000), @columnName AS NVARCHAR(5000);
+SELECT @columnName = STRING_AGG(QUOTENAME(OrderByMonth), ',') FROM VW_PivotSource;
+SET @sql = N' SELECT * FROM VW_PivotSource PIVOT ( MAX(AvgDate) FOR OrderByMonth IN ( ' 
+        + @columnName + N'))AS p ORDER BY StateProvinceName';
+
+
+
