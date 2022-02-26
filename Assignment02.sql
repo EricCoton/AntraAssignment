fda1e29a16b13317868646e9804d068b182b2202
EricCoton <94869742+EricCoton@users.noreply.github.com>
2022-02-26 16:54:01 -0500

    Update Assignment02.sql

--- Assignment02.sql
+++ Assignment02.sql
@@ -1,4 +1,7 @@
 --list customer name if primary contact peope have the same phonenumber with company
+
+USE WideWorldImporters;
+
 SELECT c.CustomerName 
 FROM Sales.Customers as C
 join Application.People as A
