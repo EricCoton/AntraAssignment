8f470e6529aa96c16a09c3033c35d01f3c1559f9
EricCoton <94869742+EricCoton@users.noreply.github.com>
2022-02-26 16:49:39 -0500

    Update Assignment01.sql

--- Assignment01.sql
+++ Assignment01.sql
@@ -1,28 +1,35 @@
---List people's full name, fax, phonenumber, company's phonenumber, and company's faxnumber if any
+--List people's full name, fax, phonenumber, company's phonenumber, and company's faxnumber if they have any
+
+USE WideWorldImporters;
 
 SELECT a.FullName, a.PhoneNumber, a.FaxNumber, b.CompanyPhoneNumber, b.CompanyFaxNumber 
 FROM Application.people AS a
-LEFT JOIN     --get all the people
-(SELECT p.PersonID, FullName, p.PhoneNumber AS PhoneNumber,p.FaxNumber AS FaxNumber, c.PhoneNumber AS CompanyPhoneNumber, c.FaxNumber AS CompanyFaxNumber
+LEFT JOIN                          --get all the people' info from people table 
+(SELECT p.PersonID, c.PhoneNumber AS CompanyPhoneNumber, c.FaxNumber AS CompanyFaxNumber
 FROM Application.People AS p
 JOIN Sales.Customers AS c
-ON p.PersonID = c.PrimaryContactPersonID OR p.PersonID = c.AlternateContactPersonID
-WHERE IsEmployee = 0               --people working for customer company
+ON p.PersonID = c.PrimaryContactPersonID
+OR p.PersonID = c.AlternateContactPersonID
+WHERE IsEmployee = 0               --get all the people working for customer company
+
 UNION
 
-SELECT PersonID, FullName, PhoneNumber, FaxNumber, PhoneNumber AS CompanyPhoneNumber, FaxNumber AS CompanyFaxNumber
+SELECT PersonID,  PhoneNumber AS CompanyPhoneNumber, FaxNumber AS CompanyFaxNumber
 FROM Application.People
-WHERE IsEmployee = 1              --people working for WWI
+WHERE IsEmployee = 1              --get all the people working for WWI company 
 
 UNION 
 
-SELECT p.PersonID, FullName, p.PhoneNumber AS PhoneNumber,p.FaxNumber AS FaxNumber, s.PhoneNumber AS CompanyPhoneNumber, s.FaxNumber AS CompanyFaxNumber
+SELECT p.PersonID,  s.PhoneNumber AS CompanyPhoneNumber, s.FaxNumber AS CompanyFaxNumber
 FROM Application.People AS p
 join Purchasing.Suppliers AS s
-ON p.PersonID = s.PrimaryContactPersonID OR p.PersonID = s.AlternateContactPersonID
-WHERE IsEmployee = 0 ) AS b       --people working for supplier company 
+ON p.PersonID = s.PrimaryContactPersonID 
+OR p.PersonID = s.AlternateContactPersonID
+WHERE IsEmployee = 0 ) AS b       --get all the people working for supplier company 
 ON a.PersonID = b.PersonID 
-WHERE a.PhoneNumber IS NOT NULL 
+WHERE a.PhoneNumber IS NOT NULL  --filter out the 'Date Conversion Only' record from people table
+
+
 
 
 
