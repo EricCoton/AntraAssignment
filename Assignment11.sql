7fdab3d99a78c7168c0e9a4e639f539f3e8c81b7
EricCoton <94869742+EricCoton@users.noreply.github.com>
2022-02-26 22:51:00 -0500

    Update Assignment11.sql

--- Assignment11.sql
+++ Assignment11.sql
@@ -1,5 +1,17 @@
 --List all the cities that were updated after 2015-01-01.
-SELECT CityName, ValidFrom, ValidTo
-From Application.Cities
-FOR SYSTEM_TIME ALL
-WHERE validFrom > '20141231 23:59:59.9999999'
+
+USE WideWorldImporters
+
+SELECT CityName, ValidFrom, ValidTo    --solution1 
+From Application.Cities  AS c
+WHERE validFrom > '20141231'
+AND CityName IN (SELECT CityName FROM Application.Cities_Archive AS ca 
+WHERE ValidTo > '20141231')
+GO
+
+
+SELECT distinct co.CityName, co.ValidFrom, co.ValidTo        --solution 2 
+FROM Application.Cities AS co
+JOIN Application.Cities_Archive AS ca 
+ON co.CityID = co.CityID AND co.ValidFrom = ca.ValidTo
+WHERE co.ValidFrom > '20141231'
