da5f48c47ecec0f80bdcc644199aa7e9d26a9d84
EricCoton <94869742+EricCoton@users.noreply.github.com>
2022-03-01 14:01:04 -0500

    Update Assignment15.sql

--- Assignment15.sql
+++ Assignment15.sql
@@ -1,4 +1,7 @@
 --List any orders that had more than one delivery attempt (located in invoice table).
+USE WideWorldImporters;
+GO
+
 select INVOICEID, JSON_VALUE(ReturnedDeliveryData, '$.Events[1].Event') AS Attemp,
 JSON_VALUE(ReturnedDeliveryData, '$.Events[1].EventTime') AS AttemptTime,
 JSON_VALUE(ReturnedDeliveryData, '$.Events[1].Status') AS DeliveredStatus,
