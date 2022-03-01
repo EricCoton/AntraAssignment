f2a8c059eb602bc77426e5b8708bfe7c6f4ed101
EricCoton <94869742+EricCoton@users.noreply.github.com>
2022-03-01 14:07:28 -0500

    Update Assignment16.sql

--- Assignment16.sql
+++ Assignment16.sql
@@ -1,4 +1,7 @@
 --List all stock items that are manufactured in China. (Country of Manufacture)
+
+USE WideWorldImporters;
+
 SELECT * FROM
 (SELECT StockItemID, StockItemName, 
 JSON_VALUE(CustomFields, '$.CountryOfManufacture') AS Country
