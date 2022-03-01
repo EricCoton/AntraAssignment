ee8e1c118f7dd3f4858ff43a9b0f6b9ab9fde6c2
EricCoton <94869742+EricCoton@users.noreply.github.com>
2022-03-01 14:28:06 -0500

    Update Assignment20.sql

--- Assignment20.sql
+++ Assignment20.sql
@@ -1,5 +1,8 @@
 /* Create a function, input: order id; return: total of that order. List invoices and use that 
 function to attach the order total to the other fields of invoices. **/
+
+USE WideWorldImporters;
+
 DROP FUNCTION IF EXISTS fn_Total_By_OrderId
 GO
 CREATE FUNCTION fn_Total_By_OrderId(@OrderID INT)
